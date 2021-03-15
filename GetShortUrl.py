1b1f110db868dd1665d46c300a43b86ca8d32deb
che <constance.he@demandforce.com>
2021-03-15 00:32:36 -0700

    'GetShortUrl'

---
+++ GetShortUrl.py
@@ -0,0 +1,24 @@
+import requests
+
+class GetShortUrl():
+
+    @staticmethod
+    def get_short_url_post(self):
+
+        url = "https://stg.ib4.me"
+
+        bearer_token = "Bearer 8900D215-88A9-DBC9-8C50-290A89UU53C6"
+        headers = {
+            "Authorization": bearer_token,
+            "Content-Type": "application/json"
+        }
+
+        data = "{ \"original_url\":\"https://www.google.com\" }"
+
+        r = requests.post(url=url, data=data, headers=headers, verify=False)
+
+        return r
+
+
+
+
